461369899cd8ac9fc07d6019ee3535894dd71dfb
Triono <triono2125@gmail.com>
2021-12-22 06:29:11 +0700

    New sources update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import Page from "./News Api";
+import './AddNewsHook.css'
+import FooterPage from './News Api - Hook/News Element/footer';
+import Search from './News Api - Hook/News Element/inputSearch';
+import NavbarHeader from './News Api - Hook/News Element/navbar';
+import NewsList from './News Api - Hook/newsPage';
 
 function App() {
   return (
     <div className="App">
-      <Page />
+      <NavbarHeader />
+      <Search />
+      <NewsList />
+      <FooterPage />
     </div>
   );
 }
